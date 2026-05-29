bf3463be5daeaf96ff55a20e0c4ca7e430cc047f
gamesworkin <gamesworkin@gmail.com>
2026-05-29 09:18:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // CONFIGURAÇÕES GERAIS - INSIRA SUAS CHAVES AQUI
 const CONFIG = {
-    ADMIN_USER: "admin",       
-    ADMIN_PASSWORD: "123",     
+    ADMIN_USER: "diegosilvaeo",       
+    ADMIN_PASSWORD: "arcnet2154",     
     YT_API_KEY: "AIzaSyATXiihPhDZohvy8mJKsAk8vjZ4WkPekmQ",
     FIREBASE_URL: "https://workin--music-default-rtdb.firebaseio.com/musicas.json" 
 };
