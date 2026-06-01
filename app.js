9d1e70cc885f879357c5e8d28fdd7e152b0e704a
gamesworkin <gamesworkin@gmail.com>
2026-06-01 14:20:56 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const USERS_DATABASE = {
     "dipriv": { 
         password: "arcnet215", 
         defaultColor: "#e74c3c",
-        firebaseUrl: "https://dipriv-47697-default-rtdb.firebaseio.com/.json",
+        firebaseUrl: "https://workin--music-default-rtdb.firebaseio.com/midias.json",
         ytApiKey: "AIzaSyD2x7SjdblFqlxQdKHlgfSZA5Nmjb1QbMk"
     }
 };
